8176f99a530a0f318f3f2790cd0d764f0faf0638
Ashirmahmatov Olmosbek <major.olmosbek.2002@gmail.com>
2024-05-23 17:05:09 +0500

    feat: add motnts

---
+++ style.js
@@ -0,0 +1,105 @@
+let month = prompt('Oyni kiriting')
+
+switch (month){
+    case "Yanvar" :{
+        console.log('Qish faslining 2-oyi')
+        break
+    }
+    case "Fevral" :{
+        console.log('Qish faslining 3-oyi')
+        break
+    }
+    case "Mart" :{
+        console.log('Bahor faslining 1-oyi')
+        break
+    }
+    case "Aprel" :{
+        console.log('Bahor faslining 2-oyi')
+        break
+    }
+    case "May" :{
+        console.log('Bahor faslining 3-oyi')
+        break
+    }
+    case "Iyun" :{
+        console.log('Yoz faslining 1-oyi')
+        break
+    }
+    case "Iyul" :{
+        console.log('Yoz faslining 2-oyi')
+        break
+    }
+    case "Avgust" :{
+        console.log('Yoz faslining 3-oyi')
+        break
+    }
+    case "Sentabr" :{
+        console.log('Kuz faslining 1-oyi')
+        break
+    }
+    case "Oktabr" :{
+        console.log('Kuz faslining 2-oyi')
+        break
+    }
+    case "Noyabr" :{
+        console.log('Kuz faslining 3-oyi')
+        break
+    }
+    case "Dekabr":{
+        console.log('Qish faslining 1-oyi')
+        break
+    }
+}
+switch (month){
+    case "12":{
+        console.log('Qish faslining Dekabr oyi')
+        break
+    }
+    case "1":{
+        console.log('Qish faslining Yanvar oyi')
+        break
+    }
+    case "2":{
+        console.log('Qish faslining Fevral oyi')
+        break
+    }
+    case "3":{
+        console.log('Bahor faslining Mart oyi')
+        break
+    }
+    case "4":{
+        console.log('Bahor faslining Aprel oyi')
+        break
+    }
+    case "5":{
+        console.log('Bahor faslining May oyi')
+        break
+    }
+    case "6":{
+        console.log('Yoz faslining Iyun oyi')
+        break
+    }
+    case "7":{
+        console.log('Yoz faslining Iyul oyi')
+        break
+    }
+    case "8":{
+        console.log('Yoz faslining Avgust oyi')
+        break
+    }
+    case "9":{
+        console.log('Kuz faslining Sentabr oyi')
+        break
+    }
+    case "10":{
+        console.log('Kuz faslining Oktabr oyi')
+        break
+    }
+    case "11":{
+        console.log('Kuz faslining Noyabr oyi')
+        break
+    }
+
+
+
+}
